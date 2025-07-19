242c996b1c2c99bcb8a115ebf383e8fdc5e56a47
Hardcore Coder <131332381+hardcore-coder-69@users.noreply.github.com>
2025-07-19 23:55:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+const shockedSoundEffectEl = document.getElementById('shocked-sound-effect');
+shockedSoundEffectEl.load();
+
+async function startVideoHandler() {
+    // await hookTextHandler();
+    await uploadHandler();
+}
+
+async function hookTextHandler() {
+    let hookTextEl = document.getElementById('hookText');
+    let hookText = hookTextEl.value;
+    if(!hookText) return;
+    const buttons = document.getElementById('buttons');
+    buttons.style.display = 'none';
+    await sleep(3);
+
+
+    let hookTextContainerEl = document.getElementById('hookTextContainer');
+    let words = hookText.split(' ').map(w => w.trim());
+    for(let i = 0; i < words.length; i++) {
+        await sleep(0.5);
+        shockedSoundEffectEl.currentTime = 0;
+        shockedSoundEffectEl.play();
+        let word = words[i];
+        let divEl = document.createElement('div');
+        divEl.classList.add('hookTextWord');
+        divEl.innerHTML = word;
+        hookTextContainerEl.appendChild(divEl);
+    }
+
+    await sleep(2);
+    await hideHookText();
+}
+
+async function hideHookText() {
+    let hookTextContainerEl = document.getElementById('hookTextContainer');
+    hookTextContainerEl.classList.add('hideHookTextClass');
+    await sleep(0.2);
+    hookTextContainerEl.remove();
+}
+
 async function uploadHandler() {
     const fileInput = document.getElementById('fileInput');
     const bgImageEl = document.getElementById('bg-image');
@@ -27,47 +68,35 @@ async function uploadHandler() {
             let imageContainer = document.getElementById("uploaded-img-container");
             let imgEl = document.getElementById('uploaded-img');
 
-            // Curtain Effect
-            let curtainCheckbox = document.getElementById("curtain-effect");
-            let curtainBox = document.getElementById("rxWorld");
-            if(curtainCheckbox && curtainCheckbox.checked) {
-                imageContainer.remove();
-                curtainBox.style.display = 'block';
-                let curtainTopEl = document.getElementById("curtain-position-top");
-                let rnInnerEl = document.getElementById("rnInner");
-                rnInnerEl.style.top = `${curtainTopEl.value}%`;
-                imgEl = document.getElementById('uploaded-img');
-            }
+            // // Shake Image
+            // let checkbox = document.getElementById("checkedBox");
+            // if (!checkbox.checked) {
+            //     imgEl.classList.remove('animate');
+            // } else {
+            //     imgEl.classList.add('animate');
+            // }
 
-            // Shake Image
-            let checkbox = document.getElementById("checkedBox");
-            if (!checkbox.checked) {
-                imgEl.classList.remove('animate');
-            } else {
-                imgEl.classList.add('animate');
-            }
+            // // Appear With Effect
+            // let appearCheckbox = document.getElementById("appear-checkbox");
+            // if (appearCheckbox.checked) {
+            //     // imageContainer.classList.add('blink');
+            //     await sleep(2);
+            //     imgEl.classList.add("appear-animation");
+            // } else {
+            //     // imageContainer.classList.remove('blink');
+            //     imgEl.classList.remove("appear-animation");
+            // }
 
-            // Appear With Effect
-            let appearCheckbox = document.getElementById("appear-checkbox");
-            if(appearCheckbox.checked) {
-                // imageContainer.classList.add('blink');
-                await sleep(2);
-                imgEl.classList.add("appear-animation");
-            } else {
-                // imageContainer.classList.remove('blink');
-                imgEl.classList.remove("appear-animation");
-            }
+            // // Burning Image
+            // let burningCheckbox = document.getElementById("burning-checkbox");
+            // if (!burningCheckbox.checked) {
+            //     imageContainer.classList.remove('burning-parent');
+            //     imgEl.classList.remove('burning-animation');
+            // } else {
+            //     imageContainer.classList.add('burning-parent');
+            //     imgEl.classList.add('burning-animation');
+            // }
 
-            // Burning Image
-            let burningCheckbox = document.getElementById("burning-checkbox");
-            if (!burningCheckbox.checked) {
-                imageContainer.classList.remove('burning-parent');
-                imgEl.classList.remove('burning-animation');
-            } else {
-                imageContainer.classList.add('burning-parent');
-                imgEl.classList.add('burning-animation');
-            }
-            
             imgEl.src = imageSrc;
             imgEl.style.display = 'none';
             imageAnimationsHandler();
@@ -83,7 +112,7 @@ async function imageAnimationsHandler() {
     imgEl.style.display = 'block';
 
     const imageStylesEl = document.getElementById("imageStyles");
-    if(imageStylesEl.value) {
+    if (imageStylesEl.value) {
         let oldStyles = imgEl.getAttribute("style");
         let updatedStyles = oldStyles + imageStylesEl.value;
         console.log(updatedStyles);
@@ -92,13 +121,13 @@ async function imageAnimationsHandler() {
 
     const transformRowsContainerEl = document.getElementById("transform-rows-container");
     const animationRows = Array.from(transformRowsContainerEl.getElementsByClassName('new-row'));
-    if(animationRows.length <= 0) return;
+    if (animationRows.length <= 0) return;
 
-    for(let i = 0; i < animationRows.length; i++) {
+    for (let i = 0; i < animationRows.length; i++) {
         let rowEl = animationRows[i];
         let animation = Array.from(rowEl.getElementsByClassName('new-animation'))[0].value;
         let duration = Array.from(rowEl.getElementsByClassName('new-animation-duration'))[0].value;
-        
+
         await sleep(0);
         console.log(animation, duration);
         imgEl.style.transition = `transform ${duration}s linear`;
@@ -131,21 +160,21 @@ async function bgAnimationsHandler() {
     imgEl.style.display = 'block';
 
     const imageStylesEl = document.getElementById("bgImageStyles");
-    if(imageStylesEl.value) {
+    if (imageStylesEl.value) {
         let oldStyles = imgEl.getAttribute("style");
         let updatedStyles = oldStyles + imageStylesEl.value;
         imgEl.setAttribute("style", updatedStyles);
     }
-    
+
     const transformRowsContainerEl = document.getElementById("transform-bgrows-container");
     const animationRows = Array.from(transformRowsContainerEl.getElementsByClassName('new-bg-row'));
-    if(animationRows.length <= 0) return;
+    if (animationRows.length <= 0) return;
 
-    for(let i = 0; i < animationRows.length; i++) {
+    for (let i = 0; i < animationRows.length; i++) {
         let rowEl = animationRows[i];
         let animation = Array.from(rowEl.getElementsByClassName('new-bg-animation'))[0].value;
         let duration = Array.from(rowEl.getElementsByClassName('new-bg-animation-duration'))[0].value;
-        
+
         await sleep(0);
         console.log(animation, duration);
         imgEl.style.transition = `transform ${duration}s linear`;
@@ -180,11 +209,11 @@ function playUploadedAudio() {
     const file = fileInput.files[0];
 
     if (file) {
-      const audioURL = URL.createObjectURL(file);
-      audioElement.src = audioURL;
-      audioElement.play();
+        const audioURL = URL.createObjectURL(file);
+        audioElement.src = audioURL;
+        audioElement.play();
     }
-  }
+}
 
 function sleep(s) {
     return new Promise(resolve => setTimeout(resolve, s * 1000));
